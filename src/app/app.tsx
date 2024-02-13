7d7e8e76dcfc8da3b8e12793cfe8699c4e5d26cb
Aleksandr <aleks.spitsin@gmail.com>
2024-02-13 14:50:09 +0300

    Setup @tanstack/react-query

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,9 @@
 import { Routing } from './routing'
+import { Providers } from './providers'
 import './app.css'
 
 export const App = () => (
-  <>
+  <Providers>
     <Routing />
-  </>
+  </Providers>
 )
